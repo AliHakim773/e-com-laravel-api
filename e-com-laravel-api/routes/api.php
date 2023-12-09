eff3c91e7c74a59b0079ceee87751abc484cf148
AliHakim773 <alihakim7732@gmail.com>
2023-12-09 14:59:30 +0200

    added delete order function route

--- e-com-laravel-api/routes/api.php
+++ e-com-laravel-api/routes/api.php
@@ -17,11 +17,12 @@ Route::controller(ProductsController::class)->group(function () {
     Route::post('add_product', 'add_product');
     Route::post('edit_product/{id}', 'edit_product');
     Route::get('get_products/{id?}', 'get_products');
-    Route::delete('delete_product/{id?}', 'delete_product');
+    Route::delete('delete_product/{id}', 'delete_product');
 });
 
 Route::controller(OrdersController::class)->group(function () {
     Route::post('add_order', 'add_order');
     Route::post('edit_order/{id}', 'edit_order');
     Route::get('get_orders/{id?}', 'get_orders');
+    Route::delete('delete_order/{id}', 'delete_order');
 });
