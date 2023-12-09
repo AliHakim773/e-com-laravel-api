2f986ecb3ef467b28ce99e52e86545bdc4489abe
AliHakim773 <alihakim7732@gmail.com>
2023-12-09 16:24:40 +0200

    added user controller with get/delete/edit user functions

---
+++ e-com-laravel-api/app/Http/Controllers/UsersController.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class UsersController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth:api');
+    }
+
+    public function edit_user(Request $req, $id)
+    {
+        $req->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:6',
+        ]);
+
+        if (!User::find($id)) {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'User not found',
+                'user_id' => $id,
+            ]);
+        }
+
+        User::where('id', $id)->update([
+            'name' => $req->name,
+            'email' => $req->email,
+            'password' => Hash::make($req->password),
+        ]);
+
+        return response()->json([
+            'status' => 'success',
+            'message' => 'User updated successfully',
+        ]);
+    }
+
+    public function get_users($id = null)
+    {
+        if ($id) {
+            return response()->json([
+                'status' => 'success',
+                'message' => 'user by id',
+                'user' => User::find($id),
+            ]);
+        } else {
+            return response()->json([
+                'status' => 'success',
+                'message' => 'list of all users',
+                'users' => User::get(),
+            ]);
+        }
+    }
+
+    public function delete_user($id)
+    {
+        $user = User::find($id);
+        if (!$user) {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'User not found',
+                'User_id' => $id,
+            ]);
+        }
+        $user->delete();
+
+        return response()->json([
+            'status' => 'success',
+            'message' => 'user deleted',
+            'user' => $user,
+        ]);
+    }
+}
