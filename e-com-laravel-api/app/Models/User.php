f96bfc77e251f1e67ed39b7db65d2f02828616a5
AliHakim773 <alihakim7732@gmail.com>
2023-12-09 13:16:15 +0200

    added relation between users and shopping carts

--- e-com-laravel-api/app/Models/User.php
+++ e-com-laravel-api/app/Models/User.php
@@ -62,4 +62,9 @@ class User extends Authenticatable implements JWTSubject
     {
         return [];
     }
+
+    public function shopingCarts()
+    {
+        return $this->hasMany(ShoppingCarts::class);
+    }
 }
