67b008a313114a7ff796473441df26d543f17fc2
AliHakim773 <alihakim7732@gmail.com>
2023-12-09 14:43:11 +0200

    added add order function

--- e-com-laravel-api/app/Http/Controllers/OrdersController.php
+++ e-com-laravel-api/app/Http/Controllers/OrdersController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Order;
+use App\Models\ShoppingCart;
 use Illuminate\Http\Request;
 
 class OrdersController extends Controller
@@ -9,5 +11,38 @@ class OrdersController extends Controller
     public function __construct()
     {
         $this->middleware('auth:api');
+    }
+
+    public function add_order(Request $req)
+    {
+        $req->validate([
+            'user_id' => 'required',
+            'product_id' => 'required',
+            'amount' => 'required',
+        ]);
+
+        $cart = ShoppingCart::where('user_id', $req->user_id)
+            ->where('status', 'open')
+            ->first();
+
+        if (!$cart) {
+            $cart = ShoppingCart::create([
+                "user_id" => $req->user_id,
+                "status" => "open",
+            ]);
+        }
+
+        $order = Order::create([
+            "user_id" => $req->user_id,
+            'product_id' =>  $req->product_id,
+            'cart_id' => $cart->id,
+            'amount' => $req->amount
+        ]);
+
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Order added successfully',
+            'order' => $order,
+        ]);
     }
 }
--- e-com-laravel-api/routes/api.php
+++ e-com-laravel-api/routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\OrdersController;
 use App\Http\Controllers\ProductsController;
 
 Route::controller(AuthController::class)->group(function () {
@@ -17,4 +18,8 @@ Route::controller(ProductsController::class)->group(function () {
     Route::post('edit_product/{id}', 'edit_product');
     Route::get('get_products/{id?}', 'get_products');
     Route::delete('delete_product/{id?}', 'delete_product');
+});
+
+Route::controller(OrdersController::class)->group(function () {
+    Route::post('add_order', 'add_order');
 });
